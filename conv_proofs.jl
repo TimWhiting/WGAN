580a0bb0a4d17a31ebd605643ffe9f1f7426149a
Evan Peterson <evanpeterson17@gmail.com>
2019-04-05 20:24:30 -0600

    Add to and refine proofs for homemade layers

--- conv_proofs.jl
+++ conv_proofs.jl
@@ -61,114 +61,121 @@
 #### EXPERIMENT 2: USE EITHER CONVOLUTION ON MNIST CLASSIFICATION ####
 ######################################################################
 
-# Classifies MNIST digits with a convolutional network.
-# Writes out saved model to the file "mnist_conv.bson".
-# Demonstrates basic model construction, training, saving,
-# conditional early-exit, and learning rate scheduling.
-
-# This model, while simple, should hit around 99% test
-# accuracy after training for approximately 20 epochs.
-
-using layers
-using learn
-using stats
-using Flux, Flux.Data.MNIST, Statistics
+using Flux, Flux.Data.MNIST, Statistics, Printf, BSON
 using Flux: onehotbatch, onecold, crossentropy, throttle
 using Base.Iterators: repeated, partition
-using Printf, BSON
 
-EXCLUDE_LABELS = false
-LABELS_TO_KEEP = [5,8]
-USE_HOMEMADE_CONV = true
+using layers, learn, stats
 
-# Load labels and images from Flux.Data.MNIST
-@info("Loading data set")
-train_labels = MNIST.labels()
-train_imgs = MNIST.images()
+function main(; useConv = true, runHomemade = true, runFlux = true, modelName = "mnist_model", epochs = 5)
 
-# Pare down the data to just two classes.
-if EXCLUDE_LABELS
-    two_class_train_indices = train_labels[map(x -> x in LABELS_TO_KEEP, train_labels)]
-    train_labels = train_labels[two_class_train_indices]
-    train_imgs = train_imgs[two_class_train_indices]
+    if useConv
+        test_set = makeTwoClassShapes((5,5))
+        train_set = [test_set]
+    else
+        # Load labels and images from Flux.Data.MNIST
+        @info("Loading data set")
+        train_labels = MNIST.labels()
+        train_imgs = MNIST.images()
+        @info("Using train set of size $(size(train_imgs))")
+
+        # Bundle images together with labels and group into minibatchess
+        batch_size = 128
+        train_set = makeMinibatches(train_imgs, train_labels, batch_size)
+
+        # Prepare test set as one giant minibatch:
+        test_imgs = MNIST.images(:test)
+        test_labels = MNIST.labels(:test)
+        @info("Using test set of size $(size(test_imgs))")
+
+        test_set = makeMinibatch(test_imgs, test_labels, 1:length(test_imgs))
+    end
+
+    # Define our model.  We will use a simple convolutional architecture with
+    # three iterations of Conv -> ReLU -> MaxPool, followed by a final Dense
+    # layer that feeds into a softmax probability output.
+    @info("Constructing model...")
+    if useConv
+        homemadeModel = Chain(
+            Convolution(3, 1, 2, σ),
+            Convolution(3, 2, 2, σ),
+            x -> reshape(x, :, size(x, 4)),
+            # x -> println("new size == $(size(x))"),
+            softmax
+        )
+        model = Chain(
+            Conv((3, 3), 1=>2, σ),
+            Conv((3, 3), 2=>2, σ),
+            x -> reshape(x, :, size(x, 4)),
+            # x -> println("new size == $(size(x))"),
+            softmax,
+        )
+    else
+        homemadeModel = Chain(
+            x -> reshape(x, 28^2, :),
+            Connected(28^2, 32, relu),
+            Connected(32, 10),
+            softmax
+        )
+        model = Chain(
+            x -> reshape(x, 28^2, :),
+            Dense(28^2, 32, relu),
+            Dense(32, 10),
+            softmax
+        )
+    end
+
+    # Load model and datasets onto GPU, if enabled
+    # train_set = gpu.(train_set)
+    # test_set = gpu.(test_set)
+    # model = gpu(model)
+
+    @info("Precompiling models...")
+    # Make sure our model is nicely precompiled before starting our training loop
+    model(train_set[1][1])
+    homemadeModel(train_set[1][1])
+
+    # `loss()` calculates the crossentropy loss between our prediction `y_hat`
+    # (calculated from `model(x)`) and the ground truth `y`.  We augment the data
+    # a bit, adding gaussian random noise to our image to make it more robust.
+    function convLoss(m, x, y)
+        # We augment `x` a little bit here, adding in random noise
+        x_aug = x .+ 0.1*gpu(randn(eltype(x), size(x)))
+        y_hat = m(x_aug)
+        return crossentropy(y_hat, y)
+    end
+
+    denseLoss(m, x, y) = crossentropy(m(x), y)
+
+    allResults::Array{LearningStats} = []
+    modelNames::Array{String} = []
+
+    if runFlux
+        results = trainOne(
+            useConv ? convLoss : denseLoss, model, train_set, test_set, ADAM(0.01),
+            modelName = "$(modelName)_results", epochs = epochs, save = false,
+            patience = 1000, lrDropThreshold = 1000, reportEvery = 10, earlyExit = false
+        )
+        # plotLearningStats(results, "$(modelName)_results", true)
+        push!(allResults, results)
+        push!(modelNames, "Flux")
+    end
+
+    if runHomemade
+        homemadeResults = trainOne(
+            useConv ? convLoss : denseLoss, homemadeModel, train_set, test_set, ADAM(0.01),
+            modelName = "$(modelName)_homemade_results", epochs = epochs, save = false,
+            patience = 1000, lrDropThreshold = 1000, reportEvery = 10, earlyExit = false
+        )
+        # plotLearningStats(homemadeResults, "$(modelName)_homemade_results", true)
+        push!(allResults, homemadeResults)
+        push!(modelNames, "Our")
+    end
+
+    plotCompareModels(allResults, modelNames, "$(modelName)_comparison")
 end
-@info("Using train set of size $(size(train_imgs))")
 
-# Bundle images together with labels and group into minibatchess
-batch_size = 128
-train_set = makeMinibatches(train_imgs, train_labels, batch_size)
-
-# Prepare test set as one giant minibatch:
-test_imgs = MNIST.images(:test)
-test_labels = MNIST.labels(:test)
-if EXCLUDE_LABELS
-    two_class_test_indices = test_labels[map(x -> x in LABELS_TO_KEEP, test_labels)]
-    test_labels = test_labels[two_class_test_indices]
-    test_imgs = test_imgs[two_class_test_indices]
-end
-@info("Using test set of size $(size(test_imgs))")
-
-test_set = makeMinibatch(test_imgs, test_labels, 1:length(test_imgs))
-
-# Define our model.  We will use a simple convolutional architecture with
-# three iterations of Conv -> ReLU -> MaxPool, followed by a final Dense
-# layer that feeds into a softmax probability output.
-@info("Constructing model...")
-if USE_HOMEMADE_CONV
-    model = Chain(
-        Convolution(5, 1, 16, 28, 28, relu),
-        Convolution(5, 16, 16, 24, 24, relu),
-        Convolution(5, 16, 10, 20, 20, relu),
-        x -> reshape(x, :, size(x, 4)),
-        # x -> println("new size == $(size(x))"),
-        Dense(2560, 10),
-        softmax
-    )
-else
-    model = Chain(
-        Conv((5, 5), 1=>16, relu),
-        Conv((5, 5), 16=>16, relu),
-        Conv((5, 5), 16=>10, relu),
-        x -> reshape(x, :, size(x, 4)),
-        # x -> println("new size == $(size(x))"),
-        Dense(2560, 10),
-        softmax,
-    )
-end
-
-# Load model and datasets onto GPU, if enabled
-# train_set = gpu.(train_set)
-# test_set = gpu.(test_set)
-# model = gpu(model)
-
-@info("Precompiling model...")
-# Make sure our model is nicely precompiled before starting our training loop
-model(train_set[1][1])
-
-# `loss()` calculates the crossentropy loss between our prediction `y_hat`
-# (calculated from `model(x)`) and the ground truth `y`.  We augment the data
-# a bit, adding gaussian random noise to our image to make it more robust.
-function loss(x, y)
-    # We augment `x` a little bit here, adding in random noise
-    x_aug = x .+ 0.1*gpu(randn(eltype(x), size(x)))
-    y_hat = model(x_aug)
-    return crossentropy(y_hat, y)
-end
-
-# Train our model with the given training set using the ADAM optimizer and
-# printing out performance against the test set as we go.
-opt = ADAM(0.001)
-evalcb = () -> @show(loss(train_set[1]...))
-
-if USE_HOMEMADE_CONV
-    modelName = "mnist_homemade_conv"
-else
-    modelName = "mnist_conv"
-end
-
-results = trainOne(loss, model, train_set, test_set, modelName = modelName, epochs = 2)
-
-plotLearningStats(results, "$(modelName)_results", true)
+main(useConv = true, runFlux = true, runHomemade = true, epochs = 500, modelName = "boxsquare_conv")
 
 # Note - Flux version of MNIST was able to get validation set accuracy of 0.9803
 # after just 2 epochs.
@@ -204,3 +211,165 @@ plotLearningStats(results, "$(modelName)_results", true)
 # @info("For the Convolution Transpose:")
 # println("Actual output:\n$(outT)")
 # println("Expected output:\n$(expectedOutT)")
+
+###########################################################################
+#### EXPERIMENT 4: USE EITHER CONVOLUTION ON BOX/SQUARE CLASSIFICATION ####
+###########################################################################
+
+# using layers
+# using learn
+# using stats
+# using datasets
+# using Flux, Statistics
+# using Flux: onehotbatch, onecold, crossentropy, mse, throttle
+# using Base.Iterators: repeated, partition
+# using Printf, BSON
+
+# function main(; useHomemadeConv = false)
+
+#     @info("Loading data set")
+#     train_set = makeTwoClassShapes((5,5))
+#     test_set = train_set
+#     println(train_set)
+#     train_set = [train_set]
+
+#     # Define our model. We will use a simple convolutional architecture with
+#     # a final Dense layer that feeds into a softmax probability output.
+#     @info("Constructing model...")
+#     if useHomemadeConv
+#         model = Chain(
+#             Convolution(5, 1, 1, σ),
+#             # Convolution(3, 2, 2, σ),
+#             x -> reshape(x, :, size(x, 4)),            
+#             # x -> println("new size == $(size(x))"),
+#             # Dense(2, 2),
+#             # softmax
+#         )
+#     else
+#         model = Chain(
+#             Conv((5, 5), 1=>1, σ),
+#             # Conv((3, 3), 2=>2, σ),
+#             x -> reshape(x, :, size(x, 4)),            
+#             # x -> println("new size == $(size(x))"),
+#             # Dense(2, 2),
+#             # softmax,
+#         )
+#     end
+
+#     @info("Precompiling model...")
+#     # Make sure our model is nicely precompiled before starting our training loop
+#     model(train_set[1][1])
+
+#     if useHomemadeConv
+#         modelName = "boxsquare_homemade_conv"
+#     else
+#         modelName = "boxsquare_conv"
+#     end
+
+#     function round(val::Flux.Tracker.TrackedReal{Float32})
+#         if val >= .5
+#             return Flux.Tracker.TrackedReal{Float32}(1.)
+#         else
+#             return Flux.Tracker.TrackedReal{Float32}(0.)
+#         end
+#     end 
+
+#     # `loss()` calculates the crossentropy loss between our prediction `y_hat`
+#     # (calculated from `model(x)`) and the ground truth `y`.  We augment the data
+#     # a bit, adding gaussian random noise to our image to make it more robust.
+#     function loss(x, y)
+#         ŷ = model(x)
+#         return mean((y .- ŷ).^2)
+#     end
+
+#     function accuracy(model, x, y)
+#         ŷ = model(x)
+#         return mean(round.(ŷ) .== y) 
+#     end
+
+#     results = trainOne(
+#         loss, model, train_set, test_set, Momentum(0.2), 
+#         modelName = modelName, epochs = 1000, save = false,
+#         lrDropThreshold = 100, earlyExit = false, reportEvery = 10,
+#         cb = () -> (), patience = 1000, showWeights = false,
+#         accuracy = accuracy
+#     )
+
+#     plotLearningStats(results, "$(modelName)_results", true)
+
+# end
+
+# main(useHomemadeConv = true)
+
+########################################################
+#### EXPERIMENT 5: TESTING LEARNING OF CONVOLUTIONS ####
+########################################################
+
+# using Flux
+# using Flux.Tracker: gradient, update!
+# using Flux: glorot_uniform
+# using LinearAlgebra, Random
+
+# using utils, layers, datasets
+
+# function main(; useFluxVersion = false)
+
+#     Random.seed!(0)
+
+#     data, labels = makeTwoClassShapes((5,5))
+#     n = size(labels)[1]
+#     data = [reshape(data[:, :, :, i], 5, 5, 1, 1) for i in 1:n]
+#     labels = [labels[i] for i in 1:n]
+#     @show data
+#     @show labels
+
+#     # data = [
+#     #     [0 0 0 0 0; 0 1 1 1 0; 0 1 1 1 0; 0 1 1 1 0; 0 0 0 0 0;],
+#     #     [0 0 0 0 0; 0 1 1 1 0; 0 1 0 1 0; 0 1 1 1 0; 0 0 0 0 0;]
+#     # ]
+#     # x = map(x -> reshape(x, 5,5,1,1), x)
+
+#     # labels = [0, 1]
+
+#     if useFluxVersion
+#         model = Chain(Conv((5,5), 1=>1))
+#     else
+#         model = Chain(Convolution(5, 1, 1))
+#     end
+
+#     function loss(x, y)
+#         ŷ = model(x)
+#         return mean((y .- ŷ).^2)
+#     end
+
+#     θ = params(model)
+#     @show θ
+
+#     if useFluxVersion
+#         @show model[1].weight
+#     else
+#         @show model[1].W    
+#     end
+
+#     η = 0.01
+#     for i = 1:1000
+#         for j in 1:2
+#             g = gradient(() -> loss(data[j], labels[j]), θ)
+#             for x in θ
+#                 update!(x, -g[x]*η)labels
+#             end
+#             if i % 100 == 0 && j == 1
+#                 println(loss(data[j], data[j]))
+#             end
+#         end
+#     end
+
+#     @show θ
+#     println("Final prediction:")
+#     @show model(data[1])
+#     println("Target:")
+#     @show labels[1]
+
+# end
+
+# main(useFluxVersion = false)
