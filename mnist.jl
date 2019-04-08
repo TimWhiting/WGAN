e732fed7b0a409c59d64e19f70de6daf032f75eb
Tim Whiting <tim@whitings.org>
2019-04-08 12:48:58 -0600

    update

--- mnist.jl
+++ mnist.jl
@@ -55,11 +55,9 @@ function DCGANGenerator(;generatorInputSize = 10)
         Dense(288, 288 * 2, relu),
         BatchNorm(288 * 2),
         x->reshape(x, 3, 3, 64, :),
-        ConvTranspose((2, 2), 64 => 64, stride = (2, 2), pad = (2, 2)),
-        BatchNorm(64, relu),
 
         # Second convolution, operating upon a 14x14 image
-        ConvTranspose((2, 2), 64 => 32, stride = (2, 2), pad = (2, 2)),
+        ConvTranspose((2, 2), 64 => 32, stride = (3, 3), pad = (2, 2)),
         BatchNorm(32, relu),
 
         # Third convolution, operating upon a 7x7 image
