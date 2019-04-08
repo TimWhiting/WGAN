c79a294998464052a3678b0f1fb3af4113cbcf1a
Tim Whiting <tim@whitings.org>
2019-04-08 12:55:48 -0600

    update

--- mnist.jl
+++ mnist.jl
@@ -52,16 +52,48 @@ end
 function DCGANGenerator(;generatorInputSize = 10)
     model = Chain(Dense(generatorInputSize, 288, relu),
         BatchNorm(288),
-        Dense(288, 288 * 2, relu),
-        BatchNorm(288 * 2),
-        x->reshape(x, 3, 3, 64, :),
+        x->reshape(x, 3, 3, 32, :),
 
         # Second convolution, operating upon a 14x14 image
-        ConvTranspose((2, 2), 64 => 32, stride = (3, 3), pad = (2, 2)),
+        ConvTranspose((3, 3), 32 => 32, stride = (2, 2), pad = (2, 2)),
         BatchNorm(32, relu),
 
         # Third convolution, operating upon a 7x7 image
-        ConvTranspose((2, 2), 32 => 1, σ, stride = (2, 2), pad = (2, 2)),
+        ConvTranspose((3, 3), 32 => 1, σ, stride = (2, 2), pad = (2, 2)),
+        x->reshape(x, 28, 28, :))
+    return DCGANGenerator(model)
+end
+
+function DCGANCritic2()
+    model = Chain(x->reshape(x, 28, 28, 1, :),
+        Conv((5, 5), 1 => 32, pad = (1, 1)),
+        BatchNorm(32, relu),
+        x->maxpool(x, (2, 2)),
+
+        # Second convolution, operating upon a 14x14 image
+        Conv((5, 5), 32 => 64, pad = (1, 1)),
+        BatchNorm(64, relu),
+        x->maxpool(x, (2, 2)),
+
+        # Reshape 3d tensor into a 2d one, at this point it should be (3, 3, 32, N)
+        # which is where we get the 288 in the `Dense` layer below:
+        x->reshape(x, 1024, :),
+        Dense(1024, 1),
+    )
+    return DCGANCritic(model)
+end
+
+function DCGANGenerator2(;generatorInputSize = 100)
+    model = Chain(Dense(generatorInputSize, 1028, relu),
+        BatchNorm(1024),
+        x->reshape(x, 7, 7, 128, :),
+
+        # Second convolution, operating upon a 14x14 image
+        ConvTranspose((4, 4), 128 => 64, stride = (2, 2), pad = (1, 1)),
+        BatchNorm(64, relu),
+
+        # Third convolution, operating upon a 7x7 image
+        ConvTranspose((4, 4), 64 => 1, σ, stride = (2, 2), pad = (1, 1)),
         x->reshape(x, 28, 28, :))
     return DCGANGenerator(model)
 end
@@ -145,7 +177,7 @@ function trainMNISTDCGAN()
     test_set = make_minibatch_mlp(test_imgs, 1:length(test_imgs))
     generatorInputSize = 20
     @info("Constructing model...")
-    wgan = WGAN(DCGANCritic(), DCGANGenerator(generatorInputSize = generatorInputSize); generatorInputSize = generatorInputSize, batchSize = batch_size)
+    wgan = WGAN(DCGANCritic2(), DCGANGenerator2(generatorInputSize = generatorInputSize); generatorInputSize = generatorInputSize, batchSize = batch_size)
    
     trainWGAN(wgan, train_set, test_set; modelName = "mnist_mlp_dcgan", numSamplesToSave = 40)
 
